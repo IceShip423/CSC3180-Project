f66dfd01e4038679d90ae03cf80c3c8e6c6ecc20
CqH <chenqiuhong45@gmail.com>
2021-04-28 23:06:34 +0800

    TSP 2

--- TSP.py
+++ TSP.py
@@ -1,167 +1,134 @@
 import random
 import math
 
-# aca parameters
-iteratorNum = 100
-antNum = 100
-p = 0.5
-q = 2
-# ......
-timeMtx = []
+p = 0.7
+alpha = 1
+beta = 5
+iteratorNum = 10
+antNum = 10
+CityNum = 10
+Citys = []
+timeMatrix = []
 pheromoneMtx = []
-maxPheromoneMtx = []
-criticalPointMtx = []
-resultData = []
 
 
-def initRandomArray(Num, LowerBound, UpperBound):
-    Array = list()
-    for i in range(Num):
-        Array.append(float(random.randint(LowerBound, UpperBound)))
-    return Array
+def distance(a, b):
+    return math.sqrt(float((b[0]-a[0]) ** 2+(b[1]-a[1]) ** 2))
 
 
-def initMtx(Rows, Cols, Val):
-    return [[Val for i in range(Cols)]for j in range(Rows)]
+def getRouteLength(tabu):
+    global p, alpha, beta, iteratorNum, antNum, CityNum, Citys, timeMatrix, pheromoneMtx
+    length = 0
+    for i in range(len(tabu)-1):
+        length += distance(Citys[tabu[i]], Citys[tabu[i+1]])
+    return length
 
 
-def initTimeMtx(tasks=[], nodes=[]):
-    global antNum, iteratorNum, p, q, timeMtx, pheromoneMtx, maxPheromoneMtx, criticalPointMtx, resultData
-    timeMtx = []
-    for i in range(0, len(tasks), 1):
-        timeMtx_i = []
-        for j in range(0, len(nodes), 1):
-            timeMtx_i.append(tasks[i]/nodes[j])
-        timeMtx.append(timeMtx_i)
+def initTimeMatrix():
+    global p, alpha, beta, iteratorNum, antNum, CityNum, Citys, timeMatrix, pheromoneMtx
+    for i in range(CityNum):
+        timeMatrix_i = []
+        for j in range(CityNum):
+            timeMatrix_i.append(distance(Citys[i], Citys[j]))
+        timeMatrix.append(timeMatrix_i)
 
 
-def initPheromoneMtx(tasks, nodes):
-    global antNum, iteratorNum, p, q, timeMtx, pheromoneMtx, maxPheromoneMtx, criticalPointMtx, resultData
-    pheromoneMtx = []
-    for i in range(0, len(tasks), 1):
-        pheromoneMtx_i = []
-        for j in range(0, len(nodes), 1):
-            pheromoneMtx_i.append(1.0)
-        pheromoneMtx.append(pheromoneMtx_i)
+def initPheromoneMtx():
+    global p, alpha, beta, iteratorNum, antNum, CityNum, Citys, timeMatrix, pheromoneMtx
+    pheromoneMtx = [[0.2 for i in range(CityNum)]for i in range(CityNum)]
 
 
-def assignOneTask(antCnt, taskCnt, nodes):
-    global antNum, iteratorNum, p, q, timeMtx, pheromoneMtx, maxPheromoneMtx, criticalPointMtx, resultData
-   # print(antCnt, "  ", criticalPointMtx, "  ",
-    #    criticalPointMtx[taskCnt], "\n")
-    if (antCnt <= criticalPointMtx[taskCnt]):
-        return maxPheromoneMtx[taskCnt]
-    else:
-        return random.randint(0, len(nodes)-1)
+def selectNextCity(tabu, allowed):
+    global p, alpha, beta, iteratorNum, antNum, CityNum, Citys, timeMatrix, pheromoneMtx
+    sum = 0.0
+    for CityCnt in allowed:
+        sum += math.pow(pheromoneMtx[tabu[len(tabu)-1]][CityCnt], alpha) * \
+            math.pow(1.0/timeMatrix[len(tabu)-1][CityCnt], beta)
+
+    pb = []
+    for i in range(CityNum):
+        flag = False
+        for j in allowed:
+            if(i == j):
+                pb.append(math.pow(pheromoneMtx[tabu[len(tabu)-1]][CityCnt], alpha) * math.pow(
+                    1.0/timeMatrix[len(tabu)-1][CityCnt], beta)/sum)
+                flag = true
+                break
+        if (flag == False):
+            pb.append(0.0)
+
+    criticalPb = random.random()
+    selectCity = 0
+    sum1 = 0.0
+    for CityCnt in range(CityNum):
+        sum1 += pb[CityCnt]
+        if (sum1 >= criticalPb):
+            selectCity = CityCnt
+            break
+
+    allowed.remove(selectCity)
+    tabu.append(selectCity)
 
 
-def callTime_oneIt(pathMtx_allAnt, tasks, nodes):
-    global antNum, iteratorNum, p, q, timeMtx, pheromoneMtx, maxPheromoneMtx, criticalPointMtx, resultData
-    time_allAnt = []
-    for antIdx in range(0, len(pathMtx_allAnt), 1):
-        pathMtx = pathMtx_allAnt[antIdx]
-        maxTime = -1
-        for nodeIdx in range(0, len(nodes), 1):
-            time = 0
-            for taskIdx in range(0, len(tasks), 1):
-                if (pathMtx[taskIdx][nodeIdx] == 1):
-                    time += timeMtx[taskIdx][nodeIdx]
-            if (time > maxTime):
-                maxTime = time
-        time_allAnt.append(maxTime)
-    return time_allAnt
-
-
-def updatePheromoneMtx(pathMtx_allAnt, timeArray_oneIt, tasks, nodes):
-    global antNum, iteratorNum, p, q, timeMtx, pheromoneMtx, maxPheromoneMtx, criticalPointMtx, resultData
-    for i in range(0, len(tasks), 1):
-        for j in range(0, len(nodes), 1):
+def updatePheromoneMtx(deltaPheromoneMtx):
+    global p, alpha, beta, iteratorNum, antNum, CityNum, Citys, timeMatrix, pheromoneMtx
+    for i in range(CityNum):
+        for j in range(CityNum):
             pheromoneMtx[i][j] *= p
-
-    minTime = 100000000
-    minIdx = -1
-    for antIdx in range(0, antNum, 1):
-        if (timeArray_oneIt[antIdx] < minTime):
-            minTime = timeArray_oneIt[antIdx]
-            minIdx = antIdx
-
-    for taskIdx in range(0, len(tasks), 1):
-        for nodeIdx in range(0, len(nodes), 1):
-            if(pathMtx_allAnt[minIdx][taskIdx][nodeIdx] == 1):
-                pheromoneMtx[taskIdx][nodeIdx] *= q
-
-    maxPheromoneMtx = []
-    criticalPointMtx = []
-    for taskIdx in range(0, len(tasks), 1):
-        maxPheromone = pheromoneMtx[taskIdx][0]
-        maxIdx = 0
-        sumPheromone = pheromoneMtx[taskIdx][0]
-        isAllSame = True
-
-        for nodeIdx in range(1, len(nodes), 1):
-            if (pheromoneMtx[taskIdx][nodeIdx] > maxPheromone):
-                maxPheromone = pheromoneMtx[taskIdx][nodeIdx]
-                maxIdx = nodeIdx
-
-            if (pheromoneMtx[taskIdx][nodeIdx] != pheromoneMtx[taskIdx][nodeIdx-1]):
-                isAllSame = False
-
-            sumPheromone += pheromoneMtx[taskIdx][nodeIdx]
-
-        if (isAllSame == True):
-            maxIdx = random.randint(0, len(nodes)-1)
-            maxPheromone = pheromoneMtx[taskIdx][maxIdx]
-
-        maxPheromoneMtx.append(maxIdx)
-        criticalPointMtx.append(int(antNum*(maxPheromone/sumPheromone)))
+    for i in range(CityNum):
+        for j in range(CityNum):
+            for antCnt in range(antNum):
+                pheromoneMtx[i][j] += deltaPheromoneMtx[k][i][j]
 
 
-def acaSearch(tasks, nodes):
-    global antNum, iteratorNum, p, q, timeMtx, pheromoneMtx, maxPheromoneMtx, criticalPointMtx, resultData
+def TspSolve():
+    global p, alpha, beta, iteratorNum, antNum, CityNum, Citys, timeMatrix, pheromoneMtx
+    bestlength = 10000000000
+    bestTour = []
     for itCnt in range(0, iteratorNum, 1):  # iteration
-        pathMtx_allAnt = []
+        deltaPheromoneMtx = []
         for antCnt in range(0, antNum, 1):  # ant-configuration
-            pathMtx_oneAnt = initMtx(len(tasks), len(nodes), 0)
-            for taskCnt in range(0, len(tasks), 1):  # task assignment
-                nodeCnt = assignOneTask(antCnt, taskCnt, nodes)
-                pathMtx_oneAnt[taskCnt][nodeCnt] = 1
-            pathMtx_allAnt.append(pathMtx_oneAnt)
-        timeArray_oneIt = callTime_oneIt(pathMtx_allAnt, tasks, nodes)
-        resultData.append(timeArray_oneIt)
-        updatePheromoneMtx(pathMtx_allAnt, timeArray_oneIt, tasks, nodes)
+
+            tabu = []
+            allowed = set(CityCnt for CityCnt in range(CityNum))
+
+            # random start
+            tabu.append(random.randint(0, CityNum-1))
+            allowed.remove(tabu[0])
+            for i in range(CityNum-1):
+                tabu.append(selectNextCity(tabu, allowed))
+            # return to first city
+            tabu.append(tabu[0])
+
+            antLength = getRouteLength(tabu)
+            if (antLength < bestLength):
+                bestLength = antLength
+                bestTour = tabu
+
+            deltaPheromoneMtx_i = [
+                [0 for i in range(CityNum)]for j in range(CityNum)]
+            for i in range(len(tabu)-1):
+                deltaPheromoneMtx_i[tabu[i]][tabu[i+1]
+                                             ] = deltaPheromoneMtx_i[tabu[i+1]][tabu[i]] = float(1.0/antLength)
+            deltaPheromoneMtx.append(deltaPheromoneMtx_i)
+        updatePheromoneMtx(deltaPheromoneMtx)
 
 
-def acaAlgorithm(tasks=[], nodes=[]):
-    global antNum, iteratorNum, p, q, timeMtx, pheromoneMtx, maxPheromoneMtx, criticalPointMtx, resultData
-    # initialization
-    initTimeMtx(tasks, nodes)
-    initPheromoneMtx(tasks, nodes)
-    criticalPointMtx = [-1 for i in range(len(tasks))]
-    # iteration & search
-    acaSearch(tasks, nodes)
-
-    # final config
-    nodeProcessTasks = []
-    for i in range(len(nodes)):
-        nodeProcessTasks_i = []
-        for j in range(len(tasks)):
-            if(maxPheromoneMtx[j] == i):
-                nodeProcessTasks_i.append(j)
-        nodeProcessTasks.append(nodeProcessTasks_i)
-
-    for i in range(len(nodes)):
-        print(i, ": ", nodeProcessTasks[i])
-
-    # history total time
-
-    print("resultData:(time for one ant)", resultData, "\n")
+for i in range(CityNum):
+    Citys.append([random.randint(0, 100), random.randint(0, 100)])
+# print("citys: ", Citys, "\n")
+initTimeMatrix()
+# print("timeMatrix: ", timeMatrix, "\n")
+initPheromoneMtx()
+print("timeMatrix: ", timeMatrix, "\n")
+TspSolve()
 
 
-# if __name__ == "main":
-# problem data
-tasks = initRandomArray(100, 10, 100)
-nodes = initRandomArray(10, 10, 100)
-print("tasks: ", tasks, "\n")
-print("nodes: ", nodes, "\n")
-# Run
-acaAlgorithm(tasks, nodes)
+# tabu = []
+# allowed = set(CityCnt for CityCnt in range(CityNum))
+# print("allowed: ", allowed, "\n")
+# print("tabu: ", tabu, "\n")
+# tabu.append(random.randint(0, CityNum-1))
+# allowed.remove(tabu[0])
+# print("allowed: ", allowed, "\n")
+# print("tabu: ", tabu, "\n")
